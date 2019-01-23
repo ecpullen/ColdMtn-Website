62a3d8b0949c31133656ec02498f5e198ab1788e
Dhruv Joshi <dhruvjoshi1998@gmail.com>
2019-01-23 16:37:28 -0500

    working on press page

---
+++ press.php
@@ -0,0 +1,44 @@
+<?php 
+include "headfoot.php";
+
+head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
+?>
+<div class="press">
+	<h1>
+		Publications including our work.
+	</h1>
+	<p>
+		Over the years, the work of Cold Mountain Builders has appeared in many publications.
+	</p><hr>
+	<div class="img-container">
+		<div class="row">
+
+			<div class="lefttwo">
+				<div class="left">
+					<a href=""><img src=""></a>
+					<a href=""><p>Open/Download PDF</p></a>
+				</div>
+				<div class="center-left">
+					<a href=""><img src=""></a>
+					<a href=""><p>Open/Download PDF</p></a>
+				</div>	
+			</div>
+
+			<div class="righttwo">		
+				<div class="center-right">
+					<a href=""><img src=""></a>
+					<a href=""><p>Open/Download PDF</p></a>
+				</div>	
+				<div class="right">
+					<a href=""><img src=""></a>
+					<a href=""><p>Open/Download PDF</p></a>
+				</div>	
+			</div>	
+
+		</div>
+	</div>
+</div>
+
+<?php
+foot();
+?>
