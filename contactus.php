dcb2a75c70b4fa2010788c8e7275e269212f84e3
ecpullen <ecpullen@aol.com>
2019-01-24 09:37:37 -0500

    Updated Contactus

--- contactus.php
+++ contactus.php
@@ -6,7 +6,7 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"contactus.css\">");
 
 <div class="contact">
 	<h1>You can find us at</h1>
-	<p>Cold Mountain Builders<br />33 Pendleton Street<br />Belfast, Maine 04915<br /><br />Office: 207.338.4552<br />Fax: 207.338.2520<br />Shop: 207.338.5565<br /><br />Email: coldmtn@coldmtn.com</p>
+	<p>Cold Mountain Builders<br />33 Pendleton Street<br />Belfast, Maine 04915<br /><br />Office: 207.338.4552<br />Fax: 207.338.2520<br />Shop: 207.338.5565<br /><br />Email: <a href="mailto:coldmtn@coldmtn.com?Subject=Cold%20Mountain%20Inquiry">coldmtn@coldmtn.com</a></p>
 </div>
 
 <?php
