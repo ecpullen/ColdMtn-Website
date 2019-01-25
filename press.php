5bafb410904a155437eded27c9a230eeedca200f
Dhruv Joshi <dhruvjoshi1998@gmail.com>
2019-01-25 16:10:34 -0500

    stylistic changes in press page

--- press.php
+++ press.php
@@ -5,7 +5,7 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
 ?>
 <div class="press_text_box">
 	<h1>
-		Publications including our work.
+		Publications that include our work.
 	</h1>
 	<p>
 		Over the years, the work of Cold Mountain Builders has appeared in many publications.
@@ -15,25 +15,25 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
 		<div class="row">
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD0807_REM_Camden_lr.pdf"><img src="press/images/press-maine-home-1.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD0807_REM_Camden_lr.pdf"><img src="press/images/press-maine-home-1.jpg"></a>
 				</div>
 				<a href="press/articles/MHD0807_REM_Camden_lr.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD0507_Remarkable_isles_lr.pdf"><img src="press/images/press-maine-home-2.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD0507_Remarkable_isles_lr.pdf"><img src="press/images/press-maine-home-2.jpg"></a>
 				</div>
 				<a href="press/articles/MHD0507_Remarkable_isles_lr.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD0508-PRO-Authentic_lr.pdf"><img src="press/images/press-maine-home-3.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD0508-PRO-Authentic_lr.pdf"><img src="press/images/press-maine-home-3.jpg"></a>
 				</div>
 				<a href="press/articles/MHD0508-PRO-Authentic_lr.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD0608-AIA_lr.pdf"><img src="press/images/press-maine-home-4.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD0608-AIA_lr.pdf"><img src="press/images/press-maine-home-4.jpg"></a>
 				</div>
 				<a href="press/articles/MHD0608-AIA_lr.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
@@ -42,25 +42,25 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
 		<div class="row">
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/DNE-JA10-Tree-House.pdf"><img src="press/images/press-design-5.jpg" width=160 height=200></a>
+					<a href="press/articles/DNE-JA10-Tree-House.pdf"><img src="press/images/press-design-5.jpg"></a>
 				</div>
 				<a href="press/articles/DNE-JA10-Tree-House.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD_Sept2012_ScenicOverlook.pdf"><img src="press/images/press-maine-home-5.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD_Sept2012_ScenicOverlook.pdf"><img src="press/images/press-maine-home-5.jpg"></a>
 				</div>
 				<a href="press/articles/MHD_Sept2012_ScenicOverlook.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MHD_Dec2012_HisAndHersHouses_TheBestLaidPlans.pdf"><img src="press/images/press-maine-home-6.jpg" width=160 height=200></a>
+					<a href="press/articles/MHD_Dec2012_HisAndHersHouses_TheBestLaidPlans.pdf"><img src="press/images/press-maine-home-6.jpg"></a>
 				</div>
 				<a href="press/articles/MHD_Dec2012_HisAndHersHouses_TheBestLaidPlans.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/Preservation-28_35_Mill_Fall14_Final.pdf"><img src="press/images/press-2015-04.jpg" width=160 height=200></a>
+					<a href="press/articles/Preservation-28_35_Mill_Fall14_Final.pdf"><img src="press/images/press-2015-04.jpg"></a>
 				</div>
 				<a href="press/articles/Preservation-28_35_Mill_Fall14_Final.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
@@ -69,25 +69,25 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
 		<div class="row">
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MaineBoats-ColdMountain48MBHH128.pdf"><img src="press/images/press-2015-05.jpg" width=160 height=200></a>
+					<a href="press/articles/MaineBoats-ColdMountain48MBHH128.pdf"><img src="press/images/press-2015-05.jpg"></a>
 				</div>
 				<a href="press/articles/MaineBoats-ColdMountain48MBHH128.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/MM_Nov2014_FREEDOM.PDF"><img src="press/images/press-2015-02.jpg" width=160 height=200></a>
+					<a href="press/articles/MM_Nov2014_FREEDOM.PDF"><img src="press/images/press-2015-02.jpg"></a>
 				</div>
 				<a href="press/articles/MM_Nov2014_FREEDOM.PDF"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/Biblioteca-Magnifica.pdf"><img src="press/images/press-2015-01.jpg" width=160 height=200></a>
+					<a href="press/articles/Biblioteca-Magnifica.pdf"><img src="press/images/press-2015-01.jpg"></a>
 				</div>
 				<a href="press/articles/Biblioteca-Magnifica.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/harbor-residence-arch-rec.pdf"><img src="press/images/harbor-residence-arch-rec.jpg" width=160 height=200></a>
+					<a href="press/articles/harbor-residence-arch-rec.pdf"><img src="press/images/harbor-residence-arch-rec.jpg"></a>
 				</div>
 				<a href="press/articles/harbor-residence-arch-rec.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
@@ -96,13 +96,13 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"press.css\">");
 		<div class="row">
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/megunticook-MBHH-LucindasLittleDreamHouse-Web.pdf"><img src="press/images/megunticook-maine-boats-homes-harbors.jpg" width=160 height=200></a>
+					<a href="press/articles/megunticook-MBHH-LucindasLittleDreamHouse-Web.pdf"><img src="press/images/megunticook-maine-boats-homes-harbors.jpg"></a>
 				</div>
 				<a href="press/articles/megunticook-MBHH-LucindasLittleDreamHouse-Web.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
 			<div class="column">
 				<div class="img_cont">
-					<a href="press/articles/mhd-2-articles.pdf"><img src="press/images/cmca-article.jpg" width=160 height=200></a>
+					<a href="press/articles/mhd-2-articles.pdf"><img src="press/images/cmca-article.jpg"></a>
 				</div>
 				<a href="press/articles/mhd-2-articles.pdf"><scan>Open/Download PDF</scan></a>
 			</div>
