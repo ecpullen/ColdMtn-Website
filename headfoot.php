5d70151e90633016ac1ed9a5722b98da40e51c78
ecpullen <ecpullen@aol.com>
2019-01-29 10:46:14 -0500

    Converted to MySql

--- headfoot.php
+++ headfoot.php
@@ -43,26 +43,32 @@ function head($stylesheet){
 		<div class="dd" id="test">
 			<a class="mainlink" href="testimonials.php">Testimonials</a>
 				<div class="d-content">
-					<a href="testimonials.php#FWCK"><span class="line">Frederic Whittmann</span> 
-								<span class="line">Christine Kondoleon</span> 
-								<span class="title">Owners</span></a>
-					<a href="testimonials.php#JG"><span class="line">John Gillespie</span> 
-								<span class="title">Architect</span></a>
-					<a href="testimonials.php#DW"><span class="line">Deborah Weisgall</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#CG"><span class="line">Christopher Glass</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#LL"><span class="line">Larry Lasser</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#LRKS"><span class="line">Liv Rockefeller</span>
-								<span class="line">Ken Shure</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#TG"><span class="line">Tony Grassi</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#BN"><span class="line">Bruce Norelius</span>
-								<span class="title">Owner</span></a>
-					<a href="testimonials.php#SVD"><span class="line">Sam Van Dam</span>
-								<span class="title">Architect</span></a>
+<?php 
+	try{
+		$db = new PDO("mysql:dbname=ecpull21;host=cs325.colby.edu",
+			"ecpull21",
+			"qyu7hbhsr");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$rows = $db->query("select * from testimonies");
+	}
+	catch(PDOException $e){
+		die($e);
+	}
+
+	foreach ($rows as $row) {
+?>
+					<a href="<?=$row["link"]?>"><span class="line"><?=$row["person1"]?></span> 
+<?php 
+		if($row["person2"]){ 
+?>
+							<span class="line"><?=$row["person2"]?></span> 
+<?php 	
+		} 
+?>
+							<span class="title"><?=$row["title"]?></span></a>
+<?php
+	}
+?>
 				</div>
 		</div>
 		<div class="dd" id="press">
--- testimonials.php
+++ testimonials.php
@@ -4,26 +4,33 @@ include "headfoot.php";
 head("<link rel=\"stylesheet\" type=\"text/css\" href=\"testimonials.css\">");
 ?>
 <div class="sidebar">
-	<a href="#FWCK"><span class="line">Frederic Whittmann</span> 
-					<span class="line">Christine Kondoleon</span> 
-					<span class="title">Owners</span></a>
-	<a href="#JG"><span class="line">John Gillespie</span> 
-				<span class="title">Architect</span></a>
-	<a href="#DW"><span class="line">Deborah Weisgall</span>
-				<span class="title">Owner</span></a>
-	<a href="#CG"><span class="line">Christopher Glass</span>
-				<span class="title">Owner</span></a>
-	<a href="#LL"><span class="line">Larry Lasser</span>
-				<span class="title">Owner</span></a>
-	<a href="#LRKS"><span class="line">Liv Rockefeller</span>
-				<span class="line">Ken Shure</span>
-				<span class="title">Owner</span></a>
-	<a href="#TG"><span class="line">Tony Grassi</span>
-				<span class="title">Owner</span></a>
-	<a href="#BN"><span class="line">Bruce Norelius</span>
-				<span class="title">Owner</span></a>
-	<a href="#SVD"><span class="line">Sam Van Dam</span>
-				<span class="title">Architect</span></a>
+<?php 
+	try{
+		$db = new PDO("mysql:dbname=ecpull21;host=cs325.colby.edu",
+			"ecpull21",
+			"qyu7hbhsr");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$rows = $db->query("select * from testimonies");
+	}
+	catch(PDOException $e){
+		die($e);
+	}
+
+	foreach ($rows as $row) {
+?>
+	<a href="<?=$row["link"]?>"><span class="line"><?=$row["person1"]?></span> 
+<?php 
+		if($row["person2"]){ 
+?>
+							<span class="line"><?=$row["person2"]?></span> 
+<?php 	
+		} 
+?>
+							<span class="title"><?=$row["title"]?></span></a>
+<?php
+	}
+	$rows->execute();
+?>
 </div>
 <div class="testimonies">
 	<div id="intro">
@@ -33,46 +40,25 @@ head("<link rel=\"stylesheet\" type=\"text/css\" href=\"testimonials.css\">");
 		</p>
 	</div>
 <?php 
-$file = fopen("testimonial.txt","r") or die("could not find file");
-$name;
-$title;
-$para;
-$loc;
-while($line = fgets($file)){
-	switch ($line[0]) {
-		case '@':
-			$title = '<span class="line" id="title">'.substr($line, 1).'</span';
-			break;
-		case '*':
-			$name .= '<span class="line">'.substr($line, 1).'</span>';
-			break;
-		case '~':
-			$para = "<p class=\"testimony\">".substr($line,1);
-			break;
-		case '#':
-			$loc = substr($line,1,strlen($string)-1);
-			break;
-		case ':':
+	foreach ($rows as $row) {
 ?>
-		<div class="loc" id='<?=$loc?>'></div>	
+		<div class="loc" id='<?=$row["id"]?>'></div>	
 		<div class="testimonial">
-			<?=$para?>
+			<p class="testimony"><?=$row["testimony"]?></p>
 			<p class = "client">
-				<?=$name?>
-				<?=$title?>
+				<span class="line"><?=$row["person1"]?></span> 
+<?php 
+		if($row["person2"]){ 
+?>
+				<span class="line"><?=$row["person2"]?></span> 
+<?php 	
+		} 
+?>
+				<span class="title"><?=$row["title"]?></span></a>
 			</p>	
 		</div>
 <?php
-			$name = "";
-			$title = "";
-			$para = "";
-			$loc = "";
-			break;
-		default:
-			$para .= $line;
-			break;
 	}
-}
 ?>
 <div class="loc"></div>
 </div>
