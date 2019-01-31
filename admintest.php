80b3c02863518bb2b60de0cade91e95bb16acd07
ecpullen <ecpullen@aol.com>
2019-01-30 23:59:49 -0500

    Update admintest.php

--- admintest.php
+++ admintest.php
@@ -102,13 +102,10 @@
 ?>
 			<div class="entry" onclick="remtest('<?=$row['id']?>')">
 				<p>
-<<<<<<< HEAD
 					<span>Testimonial: <?=htmlspecialchars($row["person1"])?> <?=htmlspecialchars($row["person2"])?> <?=htmlspecialchars($row["title"])?><br /></span>
 					<?=substr(htmlspecialchars($row["testimony"]),0,160)?> ...
-=======
 					<span class="test">Testimonial: <?=htmlspecialchars($row["person1"])?> <?=htmlspecialchars($row["person2"])?> <?=htmlspecialchars($row["title"])?><br /></span>
 					<?=substr($row["testimony"],0,97)?> ...
->>>>>>> d71516d58dacfb269a1374f553f4323044837051
 				</p>
 			</div>
 <?php
